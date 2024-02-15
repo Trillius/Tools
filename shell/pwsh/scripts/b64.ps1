4e6b467f9a1b01075b1c7737bbe56e9a00253335
Trillius <23412130+Trillius@users.noreply.github.com>
2024-02-15 19:56:04 +0000

    script tidy up

--- shell/pwsh/scripts/b64.ps1
+++ shell/pwsh/scripts/b64.ps1
@@ -1,39 +1,28 @@
 <#
-
 .SYNOPSIS
     Encodes or decodes a string using Base64 encoding.
-
 .DESCRIPTION
     This script can encode a string to Base64 format or decode a Base64 encoded string. 
     It requires a string input and a switch to specify the operation (encode or decode).
-
 .PARAMETER str
     The string to be encoded or decoded.
-
 .PARAMETER e
     A switch to indicate that the string should be encoded to Base64.
-
 .PARAMETER d
     A switch to indicate that the string should be decoded from Base64.
-
 .EXAMPLE
     .\b64.ps1 -e -str "Hello World"
     Encodes "Hello World" to Base64.
-
 .EXAMPLE
     .\b64.ps1 -d -str "SGVsbG8gV29ybGQ="
     Decodes "SGVsbG8gV29ybGQ=" from Base64 to plain text.
-
 .NOTES
     Ensure that when decoding, the input string is a valid Base64 encoded string, 
     otherwise, the script will throw an error.
     To use the command b64, add the alias b64 to the current user powershell profile (adjust path as necessary):
     Set-Alias -Name b64 -Value "$($env:UserProfile)\b64.ps1"
-    
-
 .LINK
     https://learn.microsoft.com/en-us/dotnet/api/system.convert.tobase64string?view=net-8.0
-
 #>
 
 param(
@@ -49,7 +38,7 @@ param(
     [Parameter(Mandatory = $false)]
     [switch]$h = $false
 )
-Import-Module -Name JWTDetails
+
 function Invoke-EncodeBase64($inputString) {
     $bytes = [System.Text.Encoding]::UTF8.GetBytes($inputString)
     $encodedString = [System.Convert]::ToBase64String($bytes)
@@ -61,8 +50,6 @@ function Invoke-DecodeBase64($inputString) {
     $decodedString = [System.Text.Encoding]::UTF8.GetString($bytes)
     return $decodedString
 }
-
-
 
 switch ($true) {
     { $h } {
@@ -82,21 +69,14 @@ switch ($true) {
         if ([string]::IsNullOrEmpty($str)) {
             Write-Output "No string provided for decoding."
             return
-        } 
-        if ($str.contains('.')) {
-            Write-Output " JWT token detected"
-            $output = Get-JWTDetails -token $str
-            Write-Output $output
         }
         else {
             $output = Invoke-DecodeBase64 -inputString $str
             Write-Output $output
         }
         break
-    }
-        
+    }   
     default {
         Write-Output "Please specify either -e to encode, -d to decode, or -h for help."
     }
 }
-
